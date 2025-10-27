7a3ae4618b65ecdc8bb75e2abce5b8b1847a092d
Aurio Rajaa <riorajaa2018@gmail.com>
2025-10-27 10:12:47 +0700

    Add image service, DTO, and repository
    Introduces ImageDto, ImageRepository, IImageService, and ImageService to support image upload, retrieval, update, and deletion functionalities. These additions enable handling image files associated with products, including saving images, generating download URLs, and managing image data in the database.

---
+++ src/main/java/com/dev/boxpoint/service/image/IImageService.java
@@ -0,0 +1,14 @@
+package com.dev.boxpoint.service.image;
+
+import com.dev.boxpoint.dtos.ImageDto;
+import com.dev.boxpoint.model.Image;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface IImageService {
+    Image getImageById(Long imageId);
+    void deleteImageById(Long imageId);
+    void updateImage(MultipartFile file, Long imageId);
+    List<ImageDto> saveImages(Long productId, List<MultipartFile> files);
+}
